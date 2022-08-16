5d4e0bd76c7c5d182d6c3a082d622ae8975b7fa7
Oghenero Ologe <nologe37@gmail.com>
2022-08-16 20:40:14 +0100

    first commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV;
 export const API_URL =
   env === "production"
-    ? "https://zionsfield-finances-server.herokuapp.com/"
+    ? "https://zionsfield-finances-server.herokuapp.com/api/v1/"
     : "http://localhost:9000/api/v1/";
 console.log(API_URL);
